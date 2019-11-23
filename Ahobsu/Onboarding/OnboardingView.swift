d1804d8a7749d6ed44bd31368b1e412fd3fbf123
kor-Chipmunk <rhj4862@gmail.com>
2019-11-23 16:28:13 +0900

    온보딩 뷰 컨트롤러 생성 # 1. 배경 온보딩 초기 뷰 컨트롤러 생성
    # 2. 한 것
    온보딩 뷰 컨트롤러와 온보딩 뷰 파일 생성
    
    # 3. 할 것
    1. 슬라이딩 로직 구현
    
    # 4. 메모
    추후 폴더 구조 바꾸기

---
+++ Ahobsu/Onboarding/OnboardingView.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingView.swift
+//  Ahobsu
+//
+//  Created by admin on 2019/11/23.
+//  Copyright © 2019 ahobsu. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingView: UIView {
+
+    
+    
+}
---
+++ Ahobsu/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingViewController.swift
+//  Ahobsu
+//
+//  Created by admin on 2019/11/23.
+//  Copyright © 2019 ahobsu. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
