d6ecb38ce8b1057065fee7719b8f259939ce3b8b
kor-Chipmunk <rhj4862@gmail.com>
2019-11-27 00:17:31 +0900

    fix: 첫 실행 화면 변경 ## 배경 SignInView로 첫 시작을 하던 기존 코드와 달리 SplashView로 첫 시작을 하도록 SceneDelegate.swift 코드 변경
    ## 구현한 것
    1. SceneDelegate.swift 첫 시작 코드 변경
    
    ## 메모
    SplashView 시작할 때도 WindowScene 을 넘겨줘야 할 지 고민이다.
    아마도 SplashView 에서 넘어갈 때 해줘도 되지 않을까 생각

--- Ahobsu/AppLifeCycles/SceneDelegate.swift
+++ Ahobsu/AppLifeCycles/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            let contentView = SignInView(window: window)
+            let contentView = SplashView()
             window.rootViewController = UIHostingController(rootView: contentView)
             self.window = window
             window.makeKeyAndVisible()
