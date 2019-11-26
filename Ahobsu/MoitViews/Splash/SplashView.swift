6a3c9f5081297543e3878119223fbbc8c941172f
kor-Chipmunk <rhj4862@gmail.com>
2019-11-27 01:26:52 +0900

    feat: 스플래쉬 뷰 애니메이션 추가 ## 배경 심심해보여서 애니메이션을 삽입함
    ## 구현한 것
    1. 모든 뷰가 생성될 때 애니메이션 시작
    2. 배경의 투명도 애니메이션
    3. 글씨 타이틀의 투명도 애니메이션
    4. 로고의 투명도와 글자크기 애니메이션
    5. SceneDelegate.swift 로부터 전달받은 window 객체 저장
    
    ## 보충 사항
    1. 매직 넘버 한 번에 정리하기
    
    ## 메모
    로고가 이미지 파일로 온다면, 이에 맞게 수정할 것

--- Ahobsu/MoitViews/Splash/SplashView.swift
+++ Ahobsu/MoitViews/Splash/SplashView.swift
@@ -9,41 +9,106 @@
 import SwiftUI
 
 struct SplashView: View {
+
+    @State var window: UIWindow
+
+    @State var backgroundAlpha = 0.0
+
+    @State var textAlpha = 0.0
+
+    @State var logoAlpha = 0.0
+    @State var logoScale: CGFloat = 1
+
     var body: some View {
         ZStack {
-            BackgroundView()
-            LogoView()
+            BackgroundView(backgroundAlpha: $backgroundAlpha)
+            LogoView(textAlpha: $textAlpha,
+                     logoAlpha: $logoAlpha,
+                     logoScale: $logoScale)
         }
         .edgesIgnoringSafeArea(.all)
+        .onAppear {
+            self.handleAnimations()
+        }
     }
 }
 
 struct BackgroundView: View {
+
+    @Binding var backgroundAlpha: Double
+
     var body: some View {
         Image("bgSplash")
         .resizable()
-        .aspectRatio(1, contentMode: .fit)
-        .frame(width: .infinity, height: .infinity)
+        .opacity(backgroundAlpha)
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
     }
 }
 
 struct LogoView: View {
+
+    @Binding var textAlpha: Double
+    @Binding var logoAlpha: Double
+    @Binding var logoScale: CGFloat
+
     var body: some View {
         VStack {
             Text("Moti")
             .foregroundColor(Color.white)
             .font(.system(size: 24, weight: .bold, design: .default))
+            .scaleEffect(logoScale)
+            .opacity(logoAlpha)
+            .padding(.bottom, 12)
             Text("일상의 동기를 부여하다")
-                .foregroundColor(Color.init(red: 152/1255, green: 10/255, blue: 135/255))
-                .font(.system(size: 16, weight: .bold, design: .default))
+            .foregroundColor(Color.init(.sRGB, red: 152/255, green: 10/255, blue: 135/255, opacity: 0.34))
+            .font(.system(size: 16, weight: .bold, design: .default))
+            .opacity(textAlpha)
             Spacer()
         }
         .padding(.top, 226.0)
     }
 }
 
+extension SplashView {
+
+    var backgroundAnimationDuration: Double { 1.0 }
+    var titleAnimationDuration: Double { 0.5 }
+    var logoAnimationDuration: Double { 0.3 }
+
+    func handleAnimations() {
+        runAnimationPart1()
+        runAnimationPart2()
+        runAnimationPart3()
+    }
+
+    func runAnimationPart1() {
+        withAnimation(.easeIn(duration: backgroundAnimationDuration)) {
+            backgroundAlpha = 1.0
+        }
+    }
+
+    func runAnimationPart2() {
+        let deadline: DispatchTime = .now() + backgroundAnimationDuration
+        DispatchQueue.main.asyncAfter(deadline: deadline) {
+            withAnimation(.easeIn(duration: self.titleAnimationDuration)) {
+                self.textAlpha = 1.0
+            }
+        }
+    }
+
+    func runAnimationPart3() {
+        let deadline: DispatchTime = .now() + backgroundAnimationDuration + titleAnimationDuration
+        DispatchQueue.main.asyncAfter(deadline: deadline) {
+            withAnimation(.easeIn(duration: self.logoAnimationDuration)) {
+                self.logoAlpha = 1.0
+                self.logoScale = 1.5
+            }
+        }
+    }
+}
+
 struct SplashView_Previews: PreviewProvider {
     static var previews: some View {
-        SplashView()
+        SplashView(window: UIWindow())
     }
 }
