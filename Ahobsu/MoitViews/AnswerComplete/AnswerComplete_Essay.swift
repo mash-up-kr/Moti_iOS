654d02faae43f4bc61ad2053f738d44ec3175381
kor-Chipmunk <rhj4862@gmail.com>
2019-11-29 14:49:10 +0900

    feat: 주관식 화면 업데이트 ## 배경 주관식 화면 수정된 디자인을 적용함
    ## 구현한 것
    1. 셔플 버튼 추가
    2. 입력 뷰 라운딩과 쉐도우 추가
    
    ## 보충사항
    1. 분리된 입력뷰를 텍스트에서 입력 텍스트로 바꿀 필요
    
    ## 메모
    캐릭터는 어떻게 넣어야할 지 고민

--- Ahobsu/MoitViews/AnswerComplete/AnswerComplete_Essay.swift
+++ Ahobsu/MoitViews/AnswerComplete/AnswerComplete_Essay.swift
@@ -10,23 +10,17 @@ import SwiftUI
 
 struct AnswerComplete_Essay: View {
 
-<<<<<<< HEAD
     var questionVStackSpacing: CGFloat { 16.0 }
-
+    
     var questionHStackSpacing: CGFloat { 10.0 }
-
-=======
->>>>>>> fix: 답변 완료 개별 화면 파일 구조 분리
+    
     var questionViewText: String {
         "오늘 비가와요.\n비를 주제로\n한줄 시를 써볼까요?"
     }
-
-<<<<<<< HEAD
+    
     var questionViewShuffleWidth: CGFloat { 44.0 }
     var questionViewShuffleHeight: CGFloat { 44.0 }
 
-=======
->>>>>>> fix: 답변 완료 개별 화면 파일 구조 분리
     var circleWidth: CGFloat { 268.0 }
     var circleHeight: CGFloat { 268.0 }
 
@@ -38,7 +32,6 @@ struct AnswerComplete_Essay: View {
 
     var answerPaddingTop: CGFloat { 20.0 }
     var answerMaxHeight: CGFloat { 272.0 }
-<<<<<<< HEAD
 
     var body: some View {
         ZStack {
@@ -54,18 +47,24 @@ struct AnswerComplete_Essay: View {
                         Image(systemName: "shuffle")
                     }).frame(width: questionViewShuffleWidth,
                              height: questionViewShuffleHeight)
-=======
     var answerBackgroundColor: Color {
         Color.init(red: 216/255, green: 216/255, blue: 216/255)
     }
 
     var body: some View {
         ZStack {
-            VStack(spacing: 16) {
-                HStack {
+            VStack(spacing: questionVStackSpacing) {
+                HStack(alignment: .top, spacing: questionHStackSpacing) {
                     QuestionView(text: questionViewText)
                     Spacer()
->>>>>>> fix: 답변 완료 개별 화면 파일 구조 분리
+                    Button(action: {
+                        // TODO: To add shuffle func
+                        print("Tapped")
+                    }, label: {
+                        // TODO: To change button image
+                        Image(systemName: "shuffle")
+                    }).frame(width: questionViewShuffleWidth,
+                             height: questionViewShuffleHeight)
                 }
                 Circle()
                     .fill(Color.gray)
@@ -82,15 +81,6 @@ struct AnswerComplete_Essay: View {
                     AnswerView(text: answerViewText)
                     Spacer()
                 }.padding(.top, answerPaddingTop)
-<<<<<<< HEAD
-                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: answerMaxHeight)
-                .background(Color.white)
-                .cornerRadius(10)
-                .shadow(color: Color.init(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.38), radius: 10, x: 0, y: 4)
-=======
-                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: answerMaxHeight)
-                    .background(answerBackgroundColor)
->>>>>>> fix: 답변 완료 개별 화면 파일 구조 분리
             }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .leading)
         }
         .edgesIgnoringSafeArea(.bottom)
