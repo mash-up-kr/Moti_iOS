36dcd12f6ac315c6f471a3daa2c9817378925ca5
kor-Chipmunk <rhj4862@gmail.com>
2019-12-19 01:05:57 +0900

    feat: PageControl 추가

--- Ahobsu/MotiViews/AnswerComplete/AnswerCompleteView.swift
+++ Ahobsu/MotiViews/AnswerComplete/AnswerCompleteView.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2019 ahobsu. All rights reserved.
 //
 
-import Combine
 import SwiftUI
 
 enum AnswerMode {
@@ -15,67 +14,120 @@ enum AnswerMode {
     case essayCamera
 }
 
-struct AnswerCompleteView: View {
+struct NavigationConfigurator: UIViewControllerRepresentable {
+    var configure: (UINavigationController) -> Void = { _ in }
 
-    var answerMode: AnswerMode
-
-    var contentView: some View {
-        switch answerMode {
-        case .essay:
-            return AnyView(AnswerComplete_Essay())
-        case .camera:
-            return AnyView(AnswerComplete_Camera())
-        case .essayCamera:
-            return AnyView(AnswerComplete_EssayCamera())
-        }
+    func makeUIViewController(
+        context: UIViewControllerRepresentableContext<NavigationConfigurator>) -> UIViewController {
+        UIViewController()
     }
-
-    var body: some View {
-        ZStack {
-            contentView
-            AnyView(Navigation())
+    func updateUIViewController(
+        _ uiViewController: UIViewController,
+        context: UIViewControllerRepresentableContext<NavigationConfigurator>) {
+        if let ncv = uiViewController.navigationController {
+            self.configure(ncv)
         }
     }
 }
 
-struct Navigation: View {
+struct BackgroindView: View {
+    var body: some View {
+        VStack {
+            LinearGradient(gradient: Gradient(
+                colors: [Color(UIColor.init(red: 26/255, green: 22/255, blue: 22/255, alpha: 1.0)),
+                     Color.black]),
+               startPoint: .top,
+               endPoint: .bottom
+            )
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+    }
+}
 
-    var buttonWidth: CGFloat { 44.0 }
-    var buttonHeight: CGFloat { 44.0 }
+struct AnswerCompleteView: View {
 
-    var backButton: some View {
-        Button(action: {
-            print("Tapped")
-        }, label: {
-            Text("<")
-        }).frame(width: buttonWidth, height: buttonHeight, alignment: .center)
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+
+    var answerMode: AnswerMode
+    @State var currentPage: Int
+
+//    var contentView: some View {
+//        switch answerMode {
+//        case .essay:
+//            return AnyView(AnswerComplete_Essay())
+//        case .camera:
+//            return AnyView(AnswerComplete_Camera())
+//        case .essayCamera:
+//            return AnyView(AnswerComplete_EssayCamera())
+//        }
+//    }
+
+    var btnBack : some View { Button(action: {
+        self.presentationMode.wrappedValue.dismiss()
+        }) {
+            HStack {
+            Image("icArrowLeft") // set image here
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(.white)
+            }
+        }
     }
 
     var body: some View {
-        VStack {
-            HStack {
-                backButton
-                Spacer()
-            }
-            Spacer()
+        NavigationView {
+            ZStack {
+                BackgroindView()
+                    .edgesIgnoringSafeArea([.vertical])
+                ScrollView {
+                    VStack {
+                        PageControl(numberOfPages: 7,
+                                    currentPage: $currentPage)
+                            .padding(.bottom, 16.0)
+                        HStack {
+                            Text("해커톤이 끝났어요.\n지금 기분으로\n글을 써볼까요?")
+                            .font(.custom("Baskerville", size: 24.0))
+                            .foregroundColor(Color(UIColor.rosegold))
+                            .lineSpacing(12.0)
+                            Spacer()
+                            Button(action: update) {
+                                Image("icRewriteNormal")
+                                    .renderingMode(.original)
+                                    .frame(width: 48.0, height: 48.0)
+                            }
+                        }
+                        .padding([.leading], 20.0)
+                        .padding([.trailing], 4.0)
+                        Spacer()
+                    }
+                }
+        }.navigationBarItems(leading: btnBack)
+        .navigationBarBackButtonHidden(true)
+        .navigationBarTitle(
+            Text("2019. Nov. 21")
+            .font(.custom("Baskerville", size: 24.0)), displayMode: .inline
+        )
         }
+    }
+
+    func update() {
+
     }
 }
 
 struct AnswerCompleteView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            AnswerCompleteView(answerMode: .essay)
+            AnswerCompleteView(answerMode: .essay, currentPage: 0)
                 .previewDevice(PreviewDevice(rawValue: "iPhone 11 Pro Max"))
                 .previewDisplayName("iPhone 11 Pro Max - Essay")
 
-            AnswerCompleteView(answerMode: .camera)
-                .previewDevice(PreviewDevice(rawValue: "iPhone 11 Pro Max"))
-                .previewDisplayName("iPhone 11 Pro Max - Camera")
-
-            AnswerCompleteView(answerMode: .essayCamera)
-                .previewDevice(PreviewDevice(rawValue: "iPhone 11 Pro Max"))
-                .previewDisplayName("iPhone 11 Pro Max - EssayCamera")
+//            AnswerCompleteView(answerMode: .camera)
+//                .previewDevice(PreviewDevice(rawValue: "iPhone 11 Pro Max"))
+//                .previewDisplayName("iPhone 11 Pro Max - Camera")
+//
+//            AnswerCompleteView(answerMode: .essayCamera)
+//                .previewDevice(PreviewDevice(rawValue: "iPhone 11 Pro Max"))
+//                .previewDisplayName("iPhone 11 Pro Max - EssayCamera")
         }
     }
 }
--- Ahobsu/MotiViews/AnswerComplete/AnswerComplete_Essay.swift
+++ Ahobsu/MotiViews/AnswerComplete/AnswerComplete_Essay.swift
@@ -10,67 +10,8 @@ import SwiftUI
 
 struct AnswerComplete_Essay: View {
 
-    var questionVStackSpacing: CGFloat { 16.0 }
-
-    var questionHStackSpacing: CGFloat { 10.0 }
-
-    var questionViewText: String {
-        "오늘 비가와요.\n비를 주제로\n한줄 시를 써볼까요?"
-    }
-
-    var questionViewShuffleWidth: CGFloat { 44.0 }
-    var questionViewShuffleHeight: CGFloat { 44.0 }
-
-    var circleWidth: CGFloat { 268.0 }
-    var circleHeight: CGFloat { 268.0 }
-
-    var questionPaddingTop: CGFloat { 80.0 }
-    var questionPaddingLeading: CGFloat { 15.0 }
-    var questionPaddingTrailing: CGFloat { 15.0 }
-
-    var answerViewText: String { "종로3가 순두부 식당 맛있어보인다.\n갈지 말지 고민됨\n밖은 너무 추워보임..." }
-
-    var answerPaddingTop: CGFloat { 20.0 }
-    var answerMaxHeight: CGFloat { 272.0 }
-
     var body: some View {
-        ZStack {
-            VStack(spacing: questionVStackSpacing) {
-                HStack(alignment: .top, spacing: questionHStackSpacing) {
-                    QuestionView(text: questionViewText)
-                    Spacer()
-                    Button(action: {
-                        // TODO: To add shuffle func
-                        print("Tapped")
-                    }, label: {
-                        // TODO: To change button image
-                        Image(systemName: "shuffle")
-                    }).frame(width: questionViewShuffleWidth,
-                             height: questionViewShuffleHeight)
-                }
-                Circle()
-                    .fill(Color.gray)
-                    .frame(width: circleWidth,
-                           height: circleHeight)
-                Spacer()
-            }.padding(.top, questionPaddingTop)
-                .padding(.leading, questionPaddingLeading)
-                .padding(.trailing, questionPaddingTrailing)
-
-            VStack {
-                Spacer()
-                VStack {
-                    AnswerView(text: answerViewText)
-                    Spacer()
-                }.padding(.top, answerPaddingTop)
-                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: answerMaxHeight)
-                .background(Color.white)
-                .cornerRadius(10)
-                .shadow(color: Color.init(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.38), radius: 10, x: 0, y: 4)
-            }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .leading)
-        }
-        .edgesIgnoringSafeArea(.bottom)
-        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .leading)
+        Text("Hi")
     }
 }
 
