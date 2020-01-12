72eb8f1a2634f00044c6143150d7887ced8d7ef2
kor-Chipmunk <rhj4862@gmail.com>
2020-01-12 20:24:57 +0900

    feat: 통신 API 엔드지점 추가

--- Ahobsu/Networking/AhobsuAPI.swift
+++ Ahobsu/Networking/AhobsuAPI.swift
@@ -6,13 +6,44 @@
 //  Copyright © 2019 ahobsu. All rights reserved.
 //
 
+import Foundation
+import UIKit
 import Moya
 
 enum AhobsuAPI {
-    case test
-    case testPost(postId: Int, data: String)
+    /* Answers */
+    case registerAnswer(missionId: Int, contentOrNil: String?, imageOrNil: UIImage?)
+    case updateAnswer(answerId: Int, contentOrNil: String?, imageOrNil: UIImage?)
+    case getWeekAnswers(mondayDate: String)
+    case getAnswer(date: String)
 
-    case mission
+    /* Missions */
+    case getMission
+    case refreshMission
+
+    /* SignIn */
+    case signIn(snsId: Int)
+
+    /* Token */
+    case refreshToken
+
+    /* Users */
+    case signUp(name: String,
+                birthday: String,
+                email: String,
+                gender: String,
+                snsId: Int,
+                snsType: String
+    )
+    case updateProfile(name: String,
+                birthday: String,
+                email: String,
+                gender: String,
+                snsId: Int,
+                snsType: String
+    )
+    case deleteProfile
+    case getProfile
 }
 
 extension AhobsuAPI: TargetType {
@@ -24,22 +55,76 @@ extension AhobsuAPI: TargetType {
 
     var path: String {
         switch self {
-        case .test:
-            return "/"
-        case let .testPost(postId):
-            return "/\(postId)"
-        case .mission:
+        /* Answers */
+        case .registerAnswer:
+            return "/answers"
+        case let .updateAnswer(answerId):
+            return "/answers/\(answerId)"
+        case let .getWeekAnswers(mondayDate):
+            return "/answers/week/\(mondayDate)"
+        case let .getAnswer(missionDate):
+            return "/answers/\(missionDate)"
+
+        /* Missions */
+        case .getMission:
             return "/missions"
+        case .refreshMission:
+            return "/missions/refresh"
+
+        /* SignIn */
+        case .signIn:
+            return "/signin"
+
+        /* Token */
+        case .refreshToken:
+            return "/signin/refresh"
+
+        /* Users */
+        case .signUp:
+            return "/users"
+        case .updateProfile:
+            return "/users"
+        case .deleteProfile:
+            return "/users"
+        case .getProfile:
+            return "/users/my"
         }
     }
 
-    var method: Method {
+    var method: Moya.Method {
         switch self {
-        case .test:
-            return .get
-        case .testPost:
+        /* Answers */
+        case .registerAnswer:
             return .post
-        case .mission:
+        case .updateAnswer:
+            return .put
+        case .getWeekAnswers:
+            return .get
+        case .getAnswer:
+            return .get
+
+        /* Missions */
+        case .getMission:
+            return .get
+        case .refreshMission:
+            return .get
+
+        /* SignIn */
+        case .signIn:
+            return .post
+
+        /* Token */
+        case .refreshToken:
+            return .post
+
+        /* Users */
+        case .signUp:
+            return .post
+        case .updateProfile:
+            return .put
+        case .deleteProfile:
+            return .delete
+        case .getProfile:
             return .get
         }
     }
@@ -52,11 +137,70 @@ extension AhobsuAPI: TargetType {
         var defaultParams: [String: Any] = [:]
 
         switch self {
-        case let .testPost(postId, data):
-            defaultParams["id"] = postId
-            defaultParams["data"] = data
+        /* Answers */
+        case let .registerAnswer(missionId, contentOrNil, imageOrNil):
+            defaultParams["missionId"] = missionId
+            defaultParams["content"] = contentOrNil
+            defaultParams["file"] = imageOrNil
+        case let .updateAnswer(answerId, contentOrNil, imageOrNil):
+            defaultParams["answerId"] = answerId
+            defaultParams["content"] = contentOrNil
+            defaultParams["file"] = imageOrNil
+        case .getWeekAnswers:
+            /* Empty */
+            break
+        case .getAnswer:
+            /* Empty */
+            break
 
-        default:
+        /* Missions */
+        case .getMission:
+            /* Empty */
+            break
+        case .refreshMission:
+            /* Empty */
+            break
+
+        /* SignIn */
+        case let .signIn(snsId):
+            /* Empty */
+            defaultParams["snsId"] = snsId
+
+        /* Token */
+        case .refreshToken:
+            /* Empty */
+            break
+
+        /* Users */
+        case let .signUp(name,
+                    birthday,
+                    email,
+                    gender,
+                    snsId,
+                    snsType):
+            defaultParams["name"] = name
+            defaultParams["birthday"] = birthday
+            defaultParams["email"] = email
+            defaultParams["gender"] = gender
+            defaultParams["snsId"] = snsId
+            defaultParams["snsType"] = snsType
+        case let .updateProfile(name,
+                    birthday,
+                    email,
+                    gender,
+                    snsId,
+                    snsType):
+            defaultParams["name"] = name
+            defaultParams["birthday"] = birthday
+            defaultParams["email"] = email
+            defaultParams["gender"] = gender
+            defaultParams["snsId"] = snsId
+            defaultParams["snsType"] = snsType
+        case .deleteProfile:
+            /* Empty */
+            break
+        case .getProfile:
+            /* Empty */
             break
         }
 
@@ -65,16 +209,98 @@ extension AhobsuAPI: TargetType {
 
     var task: Task {
         switch self {
-        case .testPost:
+        case let .registerAnswer(missionId, contentOrNil, imageOrNil):
+
+            var formData: [MultipartFormData] = []
+
+            if let image = imageOrNil {
+                guard let imageData = image.jpegData(compressionQuality: 1.0) else {
+                    fatalError("Cannot convert to JPEG")
+                }
+
+                if let content = contentOrNil {
+                    /* 주관식 + 사진 (혼합형) */
+                    formData.append(MultipartFormData(provider: .data(imageData),
+                    name: "file",
+                    fileName: "answer.jpeg",
+                    mimeType: "image/jpeg"))
+                    formData.append(MultipartFormData(provider: .data(Data(from: content)),
+                                                      name: "content"))
+                    formData.append(MultipartFormData(provider: .data(Data(from: missionId)),
+                                                      name: "missionId"))
+                } else {
+                    /* 사진 */
+                    formData.append(MultipartFormData(provider: .data(imageData),
+                    name: "file",
+                    fileName: "answer.jpeg",
+                    mimeType: "image/jpeg"))
+                    formData.append(MultipartFormData(provider: .data(Data(from: missionId)),
+                                                      name: "missionId"))
+                }
+            } else {
+                /* 주관식 */
+                if let content = contentOrNil {
+                    formData.append(MultipartFormData(provider: .data(Data(from: content)),
+                                                      name: "content"))
+                    formData.append(MultipartFormData(provider: .data(Data(from: missionId)),
+                                                      name: "missionId"))
+                } else {
+                    fatalError("Both Content and Image must not be nil")
+                }
+            }
+
+            return .uploadMultipart(formData)
+        case let .updateAnswer(answerId, contentOrNil, imageOrNil):
+
+            var formData: [MultipartFormData] = []
+            formData.append(MultipartFormData(provider: .data(Data(from: answerId)),
+                                              name: "answerId"))
+
+            if let image = imageOrNil {
+                guard let imageData = image.jpegData(compressionQuality: 1.0) else {
+                    fatalError("Cannot convert to JPEG")
+                }
+
+                if let content = contentOrNil {
+                    /* 주관식 + 사진 (혼합형) */
+                    formData.append(MultipartFormData(provider: .data(imageData),
+                    name: "file",
+                    fileName: "answer.jpeg",
+                    mimeType: "image/jpeg"))
+                    formData.append(MultipartFormData(provider: .data(Data(from: content)),
+                                                      name: "content"))
+                } else {
+                    /* 사진 */
+                    formData.append(MultipartFormData(provider: .data(imageData),
+                    name: "file",
+                    fileName: "answer.jpeg",
+                    mimeType: "image/jpeg"))
+                }
+            } else {
+                /* 주관식 */
+                if let content = contentOrNil {
+                    formData.append(MultipartFormData(provider: .data(Data(from: content)),
+                                                      name: "content"))
+                } else {
+                    fatalError("Both Content and Image must not be nil")
+                }
+            }
+
+            return .uploadMultipart(formData)
+        case .signIn, .updateProfile, .signUp:
             return .requestParameters(parameters: params,
-                                      encoding: URLEncoding(arrayEncoding: .noBrackets))
+                                      encoding: JSONEncoding.default)
         default:
             return .requestPlain
         }
     }
 
     var headers: [String: String]? {
-        return ["Accept": "application/json"]
+        let authToken: String = TokenManager.sharedInstance.getToken()
+
+        return ["Accept": "application/json",
+                "Content-Type": "application/json",
+                "Authorization": authToken]
     }
 
 }
