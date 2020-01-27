84a7916cd72860095779e442d1d8c04986559428
YoonJuHo <xoxoinsight@gmail.com>
2020-01-27 22:06:01 +0900

    feat: 마이 페이지 수정화면의 탈퇴하기 액션 추가

--- Ahobsu/MotiViews/MyPage/MyPageEditView.swift
+++ Ahobsu/MotiViews/MyPage/MyPageEditView.swift
@@ -7,10 +7,12 @@
 //
 
 import SwiftUI
+import Combine
 
 struct MyPageEditView: View {
 
     @Binding var user: User
+    @ObservedObject var myPageEdit = MyPageEdit()
 
     var body: some View {
         ScrollView {
@@ -24,10 +26,34 @@ struct MyPageEditView: View {
                 ListCell(title: "성별", detail: user.gender)
                 MyPageView.Separator()
                 ListCell(title: "", detail: "로그아웃")
-                ListCell(title: "", detail: "탈퇴하기").opacity(0.5)
+                Button(action: {
+                    self.myPageEdit.deleteUser()
+                }, label: {
+                    ListCell(title: "", detail: "탈퇴하기").opacity(0.5)
+                })
             }
-        }.padding(.horizontal, 15)
-        .navigationBarTitle("수정하기")
+        }
+        .padding(.horizontal, 15)
+        .navigationBarTitle("수정하기").onReceive(myPageEdit.$deletingUserSucccess) { (success) in
+            if success {
+                self.navigateRootView()
+            }
+        }
+    }
+}
+
+// Helper
+extension MyPageEditView {
+    private func navigateRootView() {
+        if let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate,
+            let window = sceneDelegate.window {
+            window.rootViewController = UIHostingController(rootView: SignInView(window: window))
+            UIView.transition(with: window,
+                              duration: 0.4,
+                              options: .transitionCrossDissolve,
+                              animations: nil,
+                              completion: nil)
+        }
     }
 }
 
