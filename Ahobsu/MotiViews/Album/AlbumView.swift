27114914f4bbd0b62ed3e859ee35e9d64b10e6ee
kor-Chipmunk <rhj4862@gmail.com>
2020-01-28 20:05:48 +0900

    feature: AlbumView 초기 설정

--- Ahobsu/MotiViews/Album/AlbumView.swift
+++ Ahobsu/MotiViews/Album/AlbumView.swift
@@ -9,16 +9,42 @@
 import SwiftUI
 
 struct AlbumView: View {
+    
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    var btnBack : some View {
+        Button(action: {
+            self.presentationMode.wrappedValue.dismiss()
+        }, label: {
+            HStack {
+                Image("icArrowLeft")
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(.white)
+            }
+        })
+    }
+    
     var body: some View {
          NavigationView {
-            ZStack {
-                BackgroundView()
+             ZStack {
+                 BackgroundView()
                     .edgesIgnoringSafeArea([.vertical])
-                ScrollView {
-                    Text("Hi")
-                }
-            }
-        }
+                 VStack {
+                     Text("Hi")
+                 }
+                 .navigationBarItems(leading: btnBack)
+                 .navigationBarBackButtonHidden(true)
+                 .navigationBarTitle(Text("앨범")
+                 .font(.custom("AppleSDGothicNeo-Regular", size: 16.0)), displayMode: .inline)
+                 .background(NavigationConfigurator { navConfig in
+                     navConfig.navigationBar.barTintColor = UIColor.init(red: 0, green: 0, blue: 0, alpha: 1)
+                     navConfig.navigationBar.titleTextAttributes = [
+                         .foregroundColor: UIColor.rosegold
+                     ]
+                 })
+             }
+             .navigationViewStyle(StackNavigationViewStyle())
+         }
     }
 }
 
