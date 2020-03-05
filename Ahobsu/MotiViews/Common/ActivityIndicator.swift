d21f9f7a6ab4e11c02b57c27f21b48d9eb382755
kor-Chipmunk <rhj4862@gmail.com>
2020-03-05 11:50:03 +0900

    feat: LoadingView 추가

---
+++ Ahobsu/MotiViews/Common/ActivityIndicator.swift
@@ -0,0 +1,24 @@
+//
+//  ActivityIndicator.swift
+//  Ahobsu
+//
+//  Created by 김선재 on 05/03/2020.
+//  Copyright © 2020 ahobsu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ActivityIndicator: UIViewRepresentable {
+    
+    @Binding var isAnimating: Bool
+    let style: UIActivityIndicatorView.Style
+    
+    func makeUIView(context: UIViewRepresentableContext<ActivityIndicator>) -> UIActivityIndicatorView {
+        return UIActivityIndicatorView(style: style)
+    }
+    
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: UIViewRepresentableContext<ActivityIndicator>) {
+        isAnimating ? uiView.startAnimating() : uiView.stopAnimating()
+    }
+    
+}
---
+++ Ahobsu/MotiViews/Common/LoadingView.swift
@@ -0,0 +1,36 @@
+//
+//  LoadingView.swift
+//  Ahobsu
+//
+//  Created by 김선재 on 05/03/2020.
+//  Copyright © 2020 ahobsu. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoadingView<Content>: View where Content: View {
+    
+    @Binding var isShowing: Bool
+    var content: () -> Content
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .center) {
+                self.content()
+                    .disabled(self.isShowing)
+                    .blur(radius: self.isShowing ? 3 : 0)
+                
+                VStack {
+                    Text("불러오는 중...")
+                    ActivityIndicator(isAnimating: .constant(true), style: .large)
+                }.frame(width: geometry.size.width / 2,
+                        height: geometry.size.height / 5)
+                    .background(Color.secondary.colorInvert())
+                    .foregroundColor(Color.primary)
+                    .cornerRadius(20)
+                    .opacity(self.isShowing ? 1 : 0)
+            }
+        }
+    }
+    
+}
