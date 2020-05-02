455473d5eab795340c6c0f83158664119606d600
kor-Chipmunk <rhj4862@gmail.com>
2020-05-03 05:44:57 +0900

    fix: CardView 리팩토링 - CardViewType Enum 타입 추가 	- 타입에 따라 자동으로 설정 	- render() 메소드로 호출 가능 - 모든 속성을 @State private var 키워드로 변경 	- 외부에서 모든 속성을 일일이 입력받는 방식은 비효율적 	- 생성자로 특정 속성만 주입받기 위해 State(또는 .init) 타입으로 값을 주입받음 - 각 속성에 맞게 메소드 추가 	- 속성들을 짝지어 정리 - 바뀐 변수명에 맞춰 뷰 코드 수정

--- Ahobsu/MotiViews/Common/CardView.swift
+++ Ahobsu/MotiViews/Common/CardView.swift
@@ -8,38 +8,130 @@
 
 import SwiftUI
 
+enum CardViewType {
+    case Main
+    case Album
+    case Option
+}
+
 struct CardView: View {
-    var isWithLine: Bool = false
-    var cornerRadius: CGFloat = 11.0
-    var shadowRadius: CGFloat = 10.0
-    var innerPaddingTop: CGFloat = 10.0
-    var innerPaddingLeading: CGFloat = 10.0
+    @State private var innerLine: Bool = false
+    
+    @State private var outterCornerRadius: CGFloat = 11.0
+    @State private var innerCornerRadius: CGFloat = 11.0
+    
+    @State private var outterShadowRadius: CGFloat = 10.0
+    @State private var outterShadowColor: Color = Color(.shadowpink)
+    
+    @State private var innerPaddingTop: CGFloat = 10.0
+    @State private var innerPaddingLeading: CGFloat = 10.0
+    
+    @State private var innerMarginTop: CGFloat = 7.0
+    @State private var innerMarginLeading: CGFloat = 2.0
+    
+    @State private var outterBorderWidth: CGFloat = 1.0
+    @State private var innerBorderWidth: CGFloat = 1.0
+    
+    @State private var outterBorderColor: Color = Color(.lightgold)
+    @State private var innerBorderColor: Color = Color(.lightgold)
+    
+    init(innerLine: Bool) {
+        self.innerLine(innerLine)
+    }
+    
+    func render(cardViewType: CardViewType) -> Self {
+        var copy = self
+        
+        switch cardViewType {
+        case .Main:
+            copy.cornerRadius()
+            copy.outterShadow()
+            copy.innerPadding()
+            copy.innerMargin()
+            copy.borderWidth()
+            copy.borderColor()
+        case .Album:
+            copy.cornerRadius(outterCornerRadius: 6.6, innerCornerRadius: 6.6)
+            copy.outterShadow(outterShadowRadius: 0.0)
+            copy.innerPadding(innerPaddingTop: 6.0, innerPaddingLeading: 6.0)
+            copy.innerMargin()
+            copy.borderWidth(outterBorderWidth: 0.6, innerBorderWidth: 0.6)
+            copy.borderColor()
+        case .Option:
+            copy.cornerRadius(outterCornerRadius: 6.6, innerCornerRadius: 6.6)
+            copy.outterShadow()
+            copy.innerPadding(innerPaddingTop: 6.0, innerPaddingLeading: 6.0)
+            copy.innerMargin()
+            copy.borderWidth(outterBorderWidth: 0.6, innerBorderWidth: 0.6)
+            copy.borderColor()
+        }
+        
+        return copy
+    }
+    
+    mutating func innerLine(_ hasInnerLine: Bool = false) {
+        self._innerLine = .init(initialValue: hasInnerLine)
+    }
+    
+    mutating func cornerRadius(outterCornerRadius: CGFloat = 11.0, innerCornerRadius: CGFloat = 11.0) {
+        self._outterCornerRadius = .init(initialValue: outterCornerRadius)
+        self._innerCornerRadius = .init(initialValue: innerCornerRadius)
+    }
+    
+    mutating func outterShadow(outterShadowRadius: CGFloat = 10.0, outterShadowColor: Color = Color(.shadowpink)) {
+        self._outterShadowRadius = .init(initialValue: outterShadowRadius)
+        self._outterShadowColor = .init(initialValue: outterShadowColor)
+    }
+    
+    mutating func innerPadding(innerPaddingTop: CGFloat = 10.0, innerPaddingLeading: CGFloat = 10.0) {
+        self._innerPaddingTop = .init(initialValue: innerPaddingTop)
+        self._innerPaddingLeading = .init(initialValue: innerPaddingLeading)
+    }
+    
+    mutating func innerMargin(innerMarginTop: CGFloat = 7.0, innerMarginLeading: CGFloat = 2.0) {
+        self._innerMarginTop = .init(initialValue: innerMarginTop)
+        self._innerMarginLeading = .init(initialValue: innerMarginLeading)
+    }
+    
+    mutating func borderWidth(outterBorderWidth: CGFloat = 1.0, innerBorderWidth: CGFloat = 1.0) {
+        self._outterBorderWidth = .init(initialValue: outterBorderWidth)
+        self._innerBorderWidth = .init(initialValue: innerBorderWidth)
+    }
+    
+    mutating func borderColor(outterBorderColor: Color = Color(.lightgold), innerBorderColor: Color = Color(.lightgold)) {
+        self._outterBorderColor = .init(initialValue: outterBorderColor)
+        self._innerBorderColor = .init(initialValue: innerBorderColor)
+    }
     
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: cornerRadius)
+            RoundedRectangle(cornerRadius: outterCornerRadius)
+                .overlay(
+                    RoundedRectangle(cornerRadius: outterCornerRadius)
+                        .stroke(outterBorderColor, lineWidth: outterBorderWidth)
+                )
                 .foregroundColor(.black)
-                .overlay(RoundedRectangle(cornerRadius: cornerRadius)
-                    .stroke(Color(.lightgold), lineWidth: 1))
-                .shadow(color: Color(.shadowpink), radius: shadowRadius / 2, x: 0, y: 0)
+                .shadow(color: outterShadowColor, radius: outterShadowRadius / 2, x: 0, y: 0)
                 .overlay(
                     ZStack {
-                        if isWithLine {
-                            
-                            RoundedRectangle(cornerRadius: cornerRadius)
-                                .overlay(RoundedRectangle(cornerRadius: cornerRadius)
-                                    .stroke(Color(.lightgold), lineWidth: 1)
-                                    .foregroundColor(.clear))
-                                .padding([.vertical], 17)
-                                .padding([.horizontal], 12)
-                                
+                        if innerLine {
+                            RoundedRectangle(cornerRadius: innerCornerRadius)
                                 .overlay(
-                                    RoundedRectangle(cornerRadius: 11)
-                                        .overlay(RoundedRectangle(cornerRadius: 11)
-                                            .stroke(Color(.lightgold), lineWidth: 1))
-                                        .foregroundColor(.clear)
-                                        .padding([.vertical], 12)
-                                        .padding([.horizontal], 17))
+                                    RoundedRectangle(cornerRadius: innerCornerRadius)
+                                        .stroke(innerBorderColor, lineWidth: innerBorderWidth)
+                                )
+                                .foregroundColor(.clear)
+                                .padding([.vertical], innerPaddingTop + innerMarginTop)
+                                .padding([.horizontal], innerPaddingLeading + innerMarginLeading)
+                                
+                            RoundedRectangle(cornerRadius: innerCornerRadius)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: innerCornerRadius)
+                                        .stroke(innerBorderColor, lineWidth: innerBorderWidth)
+                                )
+                                .foregroundColor(.clear)
+                                .padding([.vertical], innerPaddingLeading + innerMarginLeading)
+                                .padding([.horizontal], innerPaddingTop + innerMarginTop)
                         }
                     }
             )
@@ -50,7 +142,6 @@ struct CardView: View {
 
 struct CardView_Previews: PreviewProvider {
     static var previews: some View {
-        CardView(isWithLine: true)
-            .padding(60)
+        CardView(innerLine: true)
     }
 }
