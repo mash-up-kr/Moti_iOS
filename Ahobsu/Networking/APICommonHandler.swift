6a5817ace1636d9475a8433686a50124d5972414
HochanLee <1ghcks@naver.com>
2021-02-17 01:15:43 +0900

    chore: 강제 업데이트 로직 추가

--- Ahobsu/Networking/APICommonHandler.swift
+++ Ahobsu/Networking/APICommonHandler.swift
@@ -20,7 +20,7 @@ final class APICommonHandler {
         case AhobsuProvider.StatusEnum.value(.token_invalid)():
             Self.forceLogout()
         case AhobsuProvider.StatusEnum.value(.forceUpdate)():
-            break
+            Self.forceUpdate()
         default:
             break
         }
@@ -29,5 +29,26 @@ final class APICommonHandler {
     private static func forceLogout() {
         guard let window = (UIApplication.shared.windows.first { $0.tag == -1 }) else { return }
         window.rootViewController = UIHostingController(rootView: SignInView(window: window))
+    }
+    
+    static func forceUpdate() {
+        var targetViewController = UIApplication.shared.windows.first { $0.tag == -1 }?.rootViewController
+        while targetViewController?.presentedViewController != nil {
+            targetViewController = targetViewController?.presentedViewController
+        }
+        
+        let alert = UIAlertController(title: "앱의 지속적인 사용을 위해 업데이트가 필요합니다.", message: nil, preferredStyle: .alert)
+        let ok = UIAlertAction(title: "확인", style: .default) { _ in
+            let scheme = "itms-apps://itunes.apple.com/app/id1496912171?mt=8"
+            guard let schemeStr = scheme.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+                let url = URL(string: schemeStr) else { return }
+            
+            if UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url, completionHandler: nil)
+            }
+        }
+        
+        alert.addAction(ok)
+        targetViewController?.present(alert, animated: true, completion: nil)
     }
 }
