5ad65590164d2a978f61219812ecf2264c6c11ca
kor-Chipmunk <rhj4862@gmail.com>
2021-08-22 03:21:45 +0900

    fix: ZStack 으로 넣어 정렬문제 해결, 답변 완료 화면에 배경 추가

--- Ahobsu/MotiViews/Main/AnswerQuestion/AnswerRegister/AnswerRegisteredView.swift
+++ Ahobsu/MotiViews/Main/AnswerQuestion/AnswerRegister/AnswerRegisteredView.swift
@@ -11,24 +11,27 @@ import SwiftUI
 struct AnswerRegisteredView: View {
     var body: some View {
         NavigationMaskingView(isRoot: true, titleItem: Text("미션 완료"), trailingItem: EmptyView()) {
-            VStack {
-                Spacer()
-                Text("오늘의 질문에 답변을\n완료했습니다.").multilineTextAlignment(.center)
-                    .foregroundColor(Color(.rosegold))
-                    .font(.custom("IropkeBatangOTFM", size: 24.0))
-                    .lineSpacing(10.0)
-                    .padding(.bottom, 12.0)
-                Text("새로운 파츠를 얻었어요. 확인해볼까요?")
-                    .foregroundColor(Color(.rosegold))
-                    .font(.custom("IropkeBatangOTFM", size: 14.0))
-                    .lineSpacing(22.0)
-                    .padding(.bottom, 28.0)
-                Image("imgMypage")
-                Spacer()
-                MainButton(action: {
-                    self.navigateRootView()
-                }, title: "확인하러 가기")
-                Spacer()
+            ZStack {
+                BackgroundView()
+                VStack {
+                    Spacer()
+                    Text("오늘의 질문에 답변을\n완료했습니다.").multilineTextAlignment(.center)
+                        .foregroundColor(Color(.rosegold))
+                        .font(.custom("IropkeBatangOTFM", size: 24.0))
+                        .lineSpacing(10.0)
+                        .padding(.bottom, 12.0)
+                    Text("새로운 파츠를 얻었어요. 확인해볼까요?")
+                        .foregroundColor(Color(.rosegold))
+                        .font(.custom("IropkeBatangOTFM", size: 14.0))
+                        .lineSpacing(22.0)
+                        .padding(.bottom, 28.0)
+                    Image("imgMypage")
+                    Spacer()
+                    MainButton(action: {
+                        self.navigateRootView()
+                    }, title: "확인하러 가기")
+                    Spacer()
+                }
             }
         }
     }
