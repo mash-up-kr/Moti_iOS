69c45b332935eedf9831a0fe9ea93180219677d0
kor-Chipmunk <rhj4862@gmail.com>
2023-09-11 11:30:53 +0900

    refactor: 애플 로그인 시뮬레이터에서 동작하도록 SignInWithAppleButton 으로 수정

--- Ahobsu/MotiViews/SignIn/SignInView.swift
+++ Ahobsu/MotiViews/SignIn/SignInView.swift
@@ -12,7 +12,6 @@ import AuthenticationServices
 struct SignInView: View {
     
     @State var window: UIWindow
-    @State var appleSignInDelegates: SignInWithAppleDelegates! = nil
     @State var isDone = false
     @State var showingOnBordingView = true
     
@@ -33,11 +32,19 @@ struct SignInView: View {
                     }
                     .offset(x: 0, y: -100)
                     Spacer()
-                    SignWithApple()
-                        .onTapGesture(perform: showAppleLogin)
-                        .frame(height: 60, alignment: .center)
-                        .padding(.horizontal, 15)
-                        .padding(.bottom, 23)
+                    VStack {
+                        SignInWithAppleButton(
+                            onRequest: { request in
+                                request.requestedScopes = [.fullName, .email]
+                            },
+                            onCompletion: signInOnCompletion
+                        )
+                        .signInWithAppleButtonStyle(.white)
+                        .frame(height: 44, alignment: .center)
+                        .cornerRadius(22)
+                    }
+                    .padding(.horizontal, 15)
+                    .padding(.bottom, 23)
                     Text("By creating an account you are agreeing to")
                         .font(.custom("AppleSDGothicNeo-Regular", size: 12))
                         .foregroundColor(Color(UIColor.rosegold))
@@ -48,56 +55,63 @@ struct SignInView: View {
                         .underline(true, color: Color(UIColor.rosegold))
                         .onTapGesture {
                             UIApplication.shared.open(URL(string: self.linkUserAgreement)!)
-                    }.padding([.bottom], 40.0)
+                        }.padding([.bottom], 40.0)
                 }
             }
         }
         .navigationBarTitle(Text(""), displayMode: .inline)
-        .onAppear {
-            self.performExistingAccountSetupFlows()
-        }
     }
     
-    private func showAppleLogin() {
-        let request = ASAuthorizationAppleIDProvider().createRequest()
-        request.requestedScopes = [.fullName, .email]
-        
-        performSignIn(using: [request])
-    }
-    private func performExistingAccountSetupFlows() {
-        #if !targetEnvironment(simulator)
-        // Note that this won't do anything in the simulator.  You need to
-        // be on a real device or you'll just get a failure from the call.
-        let requests = [
-            ASAuthorizationAppleIDProvider().createRequest(),
-            ASAuthorizationPasswordProvider().createRequest()
-        ]
-        
-        performSignIn(using: requests)
-        #endif
-    }
-    
-    private func performSignIn(using requests: [ASAuthorizationRequest]) {
-        appleSignInDelegates = SignInWithAppleDelegates(window: window) { (success, haveAccount) in
-            if success, let haveAccount = haveAccount {
-                // update UI
-                if haveAccount {
-                    self.window.rootViewController = UIHostingController(rootView: MainView(window: self.window))
-                } else {
-                    self.window.rootViewController = UIHostingController(rootView: NavigationView { SignUpNickNameView(window: self.$window) })
-                }
-            } else {
-                // show the user an error
+    private func signInOnCompletion(result: Result<ASAuthorization, Error>) -> Void {
+        switch result {
+        case .success(let authResults):
+            switch authResults.credential{
+            case let appleIDCredential as ASAuthorizationAppleIDCredential:
+                let id = String(decoding: appleIDCredential.identityToken ?? Data(), as: UTF8.self)
+                let auth = String(decoding: appleIDCredential.authorizationCode ?? Data(), as: UTF8.self)
+                
+                AhobsuProvider.signIn(snsId: auth, auth: id, completion: { wrapper in
+                    if let signInToken = wrapper?.data {
+                        if signInToken.signUp {
+                            TokenManager.sharedInstance.registerAccessToken(token: signInToken.accessToken,
+                                                                            completion: nil,
+                                                                            error: nil)
+                            TokenManager.sharedInstance.registerRefreshToken(token: signInToken.refreshToken,
+                                                                             completion: nil,
+                                                                             error: nil)
+                        } else {
+                            TokenManager.sharedInstance.temporaryAccessToken = signInToken.accessToken
+                            TokenManager.sharedInstance.temporaryRefreshToken = signInToken.refreshToken
+                        }
+                        self.signInSucceeded(success: true, haveAccount: signInToken.signUp)
+                    } else {
+                        self.signInSucceeded(success: false, haveAccount: nil)
+                    }
+                }, error: { err in
+                    self.signInSucceeded(success: false, haveAccount: nil)
+                }, expireTokenAction: {
+                    // Empty
+                }, filteredStatusCode: nil)
+            default:
+                break
             }
+        case .failure(_):
+            self.signInSucceeded(success: false, haveAccount: nil)
         }
-        
-        let controller = ASAuthorizationController(authorizationRequests: requests)
-        controller.delegate = appleSignInDelegates
-        controller.presentationContextProvider = appleSignInDelegates
-        
-        controller.performRequests()
     }
     
+    private func signInSucceeded(success: Bool, haveAccount: Bool?) {
+        if success, let haveAccount = haveAccount {
+            // update UI
+            if haveAccount {
+                self.window.rootViewController = UIHostingController(rootView: MainView(window: self.window))
+            } else {
+                self.window.rootViewController = UIHostingController(rootView: NavigationView { SignUpNickNameView(window: self.$window) })
+            }
+        } else {
+            // show the user an error
+        }
+    }
 }
 
 struct SignInView_Previews: PreviewProvider {
